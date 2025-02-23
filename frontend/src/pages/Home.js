3f8ad04bfefc1546be3fb1b28c2c88ed4cb05126
OviKams13 <okambembo13@gmail.com>
2025-02-23 12:31:34 +0200

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts')
+      const response = await fetch('https://workout-tracking-app-backend-kgnw.onrender.com')
       const json = await response.json()
 
       if (response.ok) {
