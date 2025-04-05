f6256d0aa7199507a3759ab994cbb6cdb16ddf99
OviKams13 <okambembo13@gmail.com>
2025-04-05 10:56:49 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,13 @@ const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 const cors = require('cors')
 
-app.use(cors({ origin: "https://workout-tracking-app-frontend.onrender.com", credentials: true }));
-
 // express app
 const app = express()
+
+app.use(cors({
+  origin: 'https://workout-tracking-app-frontend.onrender.com',
+  credentials: true
+}))
 
 // middleware
 app.use(express.json())
@@ -26,7 +29,6 @@ app.use('/api/user', userRoutes)
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    // listen for requests
     app.listen(process.env.PORT, () => {
       console.log('connected to db & listening on port', process.env.PORT)
     })
